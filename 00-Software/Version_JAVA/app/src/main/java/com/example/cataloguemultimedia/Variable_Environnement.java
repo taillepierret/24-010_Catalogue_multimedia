c9286daa3c3fd7fd0de719336a538676e4041df9
Teddy <taillepierret@hotmail.fr>
2026-02-21 20:43:07 +0100

    ok

--- 00-Software/Version_JAVA/app/src/main/java/com/example/cataloguemultimedia/Variable_Environnement.java
+++ 00-Software/Version_JAVA/app/src/main/java/com/example/cataloguemultimedia/Variable_Environnement.java
@@ -3,4 +3,7 @@ package com.example.cataloguemultimedia;
 public class Variable_Environnement
 {
     public static Boolean API_is_activated = false;
+    //TODO ne marche pas depuis la premiere page, mais des qu'on passe à la deuxieme ca marche
+    //TODO pourquoi le premier item de la rehcerche à plein de meme langue ?
+    //TODO ajouter la taille de chaque fichier
 }
